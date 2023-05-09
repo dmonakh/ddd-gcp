f3ce031fb27f89c14528c2bec628e6fc106dd062
dmonakh <d.monakh@it-sprout.org>
2023-05-10 02:26:20 +0300

    delete module gke

--- terraform_gcp/gke.tf
+++
@@ -1,6 +0,0 @@
-module "gke" {
-  source = "terraform-google-modules/kubernetes-engine/google"
-  project_id = var.project_id
-  name       = "${var.def_name}-clusterk8s"
-  region     = var.region_prj
-}
