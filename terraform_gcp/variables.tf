e84d29838a79b1477f3498f823efdfdec2e79158
dmonakh <d.monakh@it-sprout.org>
2023-05-09 23:17:04 +0300

    Test deploy

---
+++ terraform_gcp/variables.tf
@@ -0,0 +1,34 @@
+variable "def_name" {
+  default = "mondyspout"
+}
+
+variable "project_id" {}
+
+variable "region_prj" {
+  default = "us-central1"
+}
+
+variable "zone_prj" {
+  default = "us-central1-a"
+}
+
+variable "sa_name" {
+  default = "sa-tfstate"
+}
+
+variable "sa_account" {
+  default = "git-490@tidy-vent-384809.iam.gserviceaccount.com"
+}
+
+variable "local_ip_range" {
+  default = "10.100.0.0/28"
+}
+
+variable "gke_ip_range" {
+  default = "10.200.0.0/19"
+}
+
+variable "roles_list" {
+  type    = list(string)
+  default = ["roles/viewer", "roles/container.admin", "roles/logging.logWriter", "roles/iam.serviceAccountAdmin", "roles/storage.admin", "roles/storage.objectAdmin"]
+}
