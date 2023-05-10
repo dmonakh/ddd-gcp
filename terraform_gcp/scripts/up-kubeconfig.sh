f38625b7217a6b01bbb10d6fcbcee977c66126fa
dmonakh <d.monakh@it-sprout.org>
2023-05-10 08:43:58 +0300

    up script

--- terraform_gcp/scripts/up-kubeconfig.sh
+++ terraform_gcp/scripts/up-kubeconfig.sh
@@ -1,4 +1,6 @@
 #!/bin/bash  
 
+gcloud components install gke-gcloud-auth-plugin
+gcloud components update
 gcloud container clusters get-credentials mondyspout-clusterk8s --region=us-central1 --project=$ARM_PROJECT_ID
 mv ~/.kube/config terraform_gcp/kubeconfig
