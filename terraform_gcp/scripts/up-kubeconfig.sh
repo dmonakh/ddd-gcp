630a78abcc6068bb480bbc977ec0c9d7f7ace30b
dmonakh <d.monakh@it-sprout.org>
2023-05-10 08:58:18 +0300

    try ls in script

--- terraform_gcp/scripts/up-kubeconfig.sh
+++ terraform_gcp/scripts/up-kubeconfig.sh
@@ -9,3 +9,4 @@ gcloud init
 gcloud components install kubectl
 gcloud container clusters get-credentials mondyspout-clusterk8s --region=us-central1 --project=$ARM_PROJECT_ID
 mv ~/.kube/config terraform_gcp/kubeconfig
+ls terraform_gcp
