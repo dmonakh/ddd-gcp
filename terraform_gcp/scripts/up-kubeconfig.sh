6a96524aa6bf2fa9d64e2ceb4f03068faa323491
dmonakh <d.monakh@it-sprout.org>
2023-05-10 08:48:16 +0300

    test

--- terraform_gcp/scripts/up-kubeconfig.sh
+++ terraform_gcp/scripts/up-kubeconfig.sh
@@ -2,6 +2,6 @@
 
 # gcloud components install gke-gcloud-auth-plugin
 # gcloud components update
-sudo apt-get install google-cloud-sdk-gke-gcloud-auth-plugin
-gcloud container clusters get-credentials mondyspout-clusterk8s --region=us-central1 --project=$ARM_PROJECT_ID
+# sudo apt-get install google-cloud-sdk-gke-gcloud-auth-plugin
+gcloud beta container clusters get-credentials mondyspout-clusterk8s --region=us-central1 --project=$ARM_PROJECT_ID
 mv ~/.kube/config terraform_gcp/kubeconfig
