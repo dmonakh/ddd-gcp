6d6bd2a7288ed57900b7b15e2de886f16c474d01
dmonakh <d.monakh@it-sprout.org>
2023-05-10 09:17:05 +0300

    test

--- terraform_gcp/scripts/up-kubeconfig.sh
+++ terraform_gcp/scripts/up-kubeconfig.sh
@@ -11,7 +11,8 @@
 sudo apt-get install apt-transport-https ca-certificates gnupg
 echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] https://packages.cloud.google.com/apt cloud-sdk main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
 curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key --keyring /usr/share/keyrings/cloud.google.gpg add -
-sudo apt-get update && sudo apt-get install google-cloud-cli
+# sudo apt-get update && sudo apt-get install google-cloud-cli
+sudo apt-get install google-cloud-sdk-gke-gcloud-auth-plugin
 gcloud container clusters get-credentials mondyspout-clusterk8s --region=us-central1 --project=$ARM_PROJECT_ID
 # mv ~/.kube/config terraform_gcp/kubeconfig
 # ls terraform_gcp
