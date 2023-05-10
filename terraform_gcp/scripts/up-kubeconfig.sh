de87f423e47c6762c576067c73250fc777a0e30e
dmonakh <d.monakh@it-sprout.org>
2023-05-10 09:11:02 +0300

    test get cred cluster

--- terraform_gcp/scripts/up-kubeconfig.sh
+++ terraform_gcp/scripts/up-kubeconfig.sh
@@ -3,12 +3,12 @@
 # gcloud components install gke-gcloud-auth-plugin
 # gcloud components update
 # sudo apt-get install google-cloud-sdk-gke-gcloud-auth-plugin
-curl https://sdk.cloud.google.com | bash
-source ~/.bashrc
-gcloud init
-gcloud components install kubectl
+# curl https://sdk.cloud.google.com | bash
+# source ~/.bashrc
+# gcloud init
+# gcloud components install kubectl
 
 
-# gcloud container clusters get-credentials mondyspout-clusterk8s --region=us-central1 --project=$ARM_PROJECT_ID
+gcloud container clusters get-credentials mondyspout-clusterk8s --region=us-central1 --project=$ARM_PROJECT_ID
 # mv ~/.kube/config terraform_gcp/kubeconfig
-ls terraform_gcp
+# ls terraform_gcp
