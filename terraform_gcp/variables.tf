ddd052b8f4e0ce75fca4ff98bf4184d9813b41a5
dmonakh <d.monakh@it-sprout.org>
2023-05-13 11:57:42 +0300

    mv service acc

--- terraform_gcp/variables.tf
+++ terraform_gcp/variables.tf
@@ -28,7 +28,7 @@ variable "gke_ip_range" {
   default = "10.200.0.0/19"
 }
 
-variable "roles_list" {
-  type    = list(string)
-  default = ["roles/viewer", "roles/container.admin", "roles/logging.logWriter", "roles/iam.serviceAccountAdmin", "roles/storage.admin", "roles/storage.objectAdmin"]
-}
+# variable "roles_list" {
+#   type    = list(string)
+#   default = ["roles/viewer", "roles/container.admin", "roles/logging.logWriter", "roles/iam.serviceAccountAdmin", "roles/storage.admin", "roles/storage.objectAdmin"]
+# }
